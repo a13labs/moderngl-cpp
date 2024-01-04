ebf7ea4884da9a775575ec52ccc935fe1f2a267f
Alexandre Pires <c.alexandre.pires@gmail.com>
2024-01-04 19:02:18 +0100

    Minor changes

--- mgl/window/inc/mgl_window/api.hpp
+++ mgl/window/inc/mgl_window/api.hpp
@@ -4,11 +4,11 @@
 
 namespace mgl::window::api
 {
-  // Map the opengl namespace to the window namespace, eventually we might have a vulkan namespace
+  // Map the opengl namespace to the api namespace, eventually we might have a vulkan api
   using context_ref = mgl::opengl::context_ref;
   using texture_ref = mgl::opengl::texture_ref;
   using program_ref = mgl::opengl::program_ref;
   using uniform_ref = mgl::opengl::uniform_ref;
   using buffer_ref = mgl::opengl::buffer_ref;
 
-}; // namespace mgl::window
+}; // namespace mgl::window::api
